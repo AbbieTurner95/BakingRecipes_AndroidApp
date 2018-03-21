3090c91e53fb4b9672c6ccca07345c7461e54129
Abbie <abbieturner95@hotmail.com>
2018-03-21 16:14:28 +0000

    commit

--- app/src/main/java/android/example/com/bakingapp/DetailedStepsActivity.java
+++ app/src/main/java/android/example/com/bakingapp/DetailedStepsActivity.java
@@ -2,6 +2,7 @@ package android.example.com.bakingapp;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -15,6 +16,14 @@ public class DetailedStepsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps_fragment);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        DetailedStepsFragment detailedStepsFragment = new DetailedStepsFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.recipe_step_detail_framelayout, detailedStepsFragment)
+                .commit();
 
 
     }
